973c9269ac2433bd8b0016c7705d0223325955bd
test <3402502077@qq.com>
2024-05-10 23:34:48 +0800

    2.0

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * @Author: test 3402502077@qq.com
  * @Date: 2024-05-10 21:59:13
  * @LastEditors: test 3402502077@qq.com
- * @LastEditTime: 2024-05-10 23:33:38
+ * @LastEditTime: 2024-05-10 23:34:45
  * @FilePath: \LED of flowing water\src\main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,6 +13,7 @@ void setup() {
 for(int i = 2; i < 8; i++)
 pinMode(i,OUTPUT);
 }
+
 
 void loop() {
   //来回for循环，设置好起止点
